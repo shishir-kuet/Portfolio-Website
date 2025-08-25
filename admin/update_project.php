ad417b39341b3ac04897146fc50582459309ecae
Md. Mosaddek Ali <124651785+shishir-kuet@users.noreply.github.com>
2025-08-25 19:21:32 +0600

    Create update_project.php

---
+++ admin/update_project.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: index.html");
+    exit();
+}
+include 'db_connect.php';
+
+$id = $_POST['id'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+$link = $_POST['link'];
+
+$stmt = $conn->prepare("UPDATE projects SET title=?, description=?, link=? WHERE id=?");
+$stmt->bind_param("sssi", $title, $desc, $link, $id);
+$stmt->execute();
+
+header("Location: admin.php");
+exit();
+?>
